815f596e4a46804b825accfd12998a66eab51b2b
KylinSuzihe <3156975183@qq.com>
2023-06-05 14:30:50 +0800

    定义领用、借用dao层

---
+++ src/main/java/dao/impl/get_or_borrow_dao_impl.java
@@ -0,0 +1,43 @@
+package dao.impl;
+
+import dao.get_or_borrow_dao;
+import domain.get_or_borrow_Requisition;
+
+import java.util.List;
+
+public class get_or_borrow_dao_impl implements get_or_borrow_dao {
+    @Override
+    public void createTable(get_or_borrow_Requisition table) {
+
+    }
+
+    @Override
+    public void updateTable(get_or_borrow_Requisition table) {
+
+    }
+
+    @Override
+    public void deleteTable(String tableId) {
+
+    }
+
+    @Override
+    public void submitTable(String tableId) {
+
+    }
+
+    @Override
+    public get_or_borrow_Requisition searchTableById(String tableId) {
+        return null;
+    }
+
+    @Override
+    public List<get_or_borrow_Requisition> searchTableByUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<get_or_borrow_Requisition> searchTableWhichApproved() {
+        return null;
+    }
+}
